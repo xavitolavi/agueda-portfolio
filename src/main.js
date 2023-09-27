bef4391757084d274f0fe7d4b2d9247d74f7b66b
Xavi Florit <xavi.florit@inari.io>
2023-09-27 12:11:15 +0200

    removed vue-plyr

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VuePlyr from 'vue-plyr'
-import 'vue-plyr/dist/vue-plyr.css'
 
 createApp(App)
-  .use(VuePlyr, {
-    plyr: {}
-  })
   .mount('#app')
